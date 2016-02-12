3681651ab52571f75a1e0030aa0f78191f541b22
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-02-12 10:38:28 -0800

    Add code

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -4,33 +4,67 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Sami Zheng].
 
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
+=begin
+def total(Array)
+  For each item in array
+    add each item together
+  print result
+  end
+end
+=end
 
-# Input:
-# Output:
+# Input: Array
+# Output: Sum of Array
 # Steps to solve the problem.
-
+# 1. Iterate over each item in Array
+# 2. Add each item together
+# 3. Print result
 
 # 1. total initial solution
-
+def total(array)
+  counter = 0
+  array.each do |item|
+    counter += item
+  end
+  p counter
+end
 
 
 # 3. total refactored solution
-
-
+def total(array)
+  array.inject(0) {|sum, i| p sum + i}
+end
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
+# Input: Array
+# Output: Sentence
 # Steps to solve the problem.
-
+=begin
+def sentence_maker(array)
+  for each item in array
+    combine items to make cohesive sentence
+  end
+end
+=end
 
 # 5. sentence_maker initial solution
 
-
+def sentence_maker(array)
+  sentence = ""
+  array.each do |item|
+    sentence += item + " "
+  end
+  sentence.capitalize!
+  p sentence + "."
+end
 
 # 6. sentence_maker refactored solution
+
+def sentence_maker(array)
+  p array.join(" ").capitalize << "."
+end
