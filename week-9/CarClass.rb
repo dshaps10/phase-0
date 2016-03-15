f01919414f7cd340785c127b33c0fb4cf173c623
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-03-15 13:52:25 -0700

    Add initial code

---
+++ week-9/CarClass.rb
@@ -0,0 +1,47 @@
+# Create a Car Class from User Stories
+
+
+# I worked on this challenge [by myself].
+
+
+# 2. Pseudocode
+
+
+
+# 3. Initial Solution
+Class car
+
+    def initialize(model, color)
+        @model = model
+        @color = color
+    end
+
+    def distance=(distance)
+        @distance = distance
+    end
+
+    def speed
+        @speed
+    end
+
+
+
+end
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection
